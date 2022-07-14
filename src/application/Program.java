71ddc1a7afb3db87118b815dc9465e3c1cadc78b
Luiz Carlos <luizcarlospedronijr@yahoo.com.br>
2022-07-14 16:56:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,10 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 
-
+		System.out.println("\n=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 }
